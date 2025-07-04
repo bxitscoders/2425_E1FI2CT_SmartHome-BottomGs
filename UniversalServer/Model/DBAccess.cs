c73d5c408a3fa853ecca10177d46aa3e4afbf235
ozy <thisisozy.personal@gmail.com>
2025-07-04 12:03:27 +0200

    Version 2 with 2 tables

--- UniversalServer/Model/DBAccess.cs
+++ UniversalServer/Model/DBAccess.cs
@@ -39,28 +39,78 @@ namespace UniversalServer.Model
 
         public void InsertData(TempValue tv, HumidValue hv, PressureValue pv, DateTime dt, string ip)
         {
-            string insertCmd = $@"
-                INSERT INTO tbl_werte (
+            var sqlCmd = _myConnection.CreateCommand();
+
+            int? sens_id = null;
+
+            // Select sensor id if exists
+            sqlCmd.CommandText = @"
+                SELECT id
+                FROM tbl_sensors
+                WHERE ip = @ip;
+            ";
+            sqlCmd.Parameters.AddWithValue("@ip", ip);
+
+            object result = sqlCmd.ExecuteScalar();
+            if (result != null && result != DBNull.Value)
+            {
+                sens_id = Convert.ToInt32(result);
+            }
+            else
+            {
+                // Insert new sensor
+                sqlCmd.CommandText = @"
+                    INSERT INTO tbl_sensors (
+                        room,
+                        ip,
+                        code
+                    ) VALUES (
+                        @room,
+                        @ip,
+                        @code
+                    );
+                ";
+
+                sqlCmd.Parameters.Clear();
+                sqlCmd.Parameters.AddWithValue("@room", "Placeholder Room");
+                sqlCmd.Parameters.AddWithValue("@ip", ip);
+                sqlCmd.Parameters.AddWithValue("@code", "print(\"Django unchained!\")");
+
+                sqlCmd.ExecuteNonQuery();
+
+                // Get last inserted id
+                sqlCmd.CommandText = "SELECT last_insert_rowid();";
+                sqlCmd.Parameters.Clear();
+
+                sens_id = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                Console.WriteLine(sens_id);
+            }
+
+            // Insert values
+            sqlCmd.CommandText = @"
+                INSERT INTO tbl_values (
                     temperature,
                     humidity,
                     pressure,
                     date,
-                    ipadress
-                )
-                VALUES (
-                    {tv.Value.ToString().Replace(',','.')},
-                    {hv.Value.ToString().Replace(',', '.')},
-                    {pv.Value.ToString().Replace(',', '.')},
-                    '{dt.ToString("yyyy-MM-dd HH:mm:ss")}',
-                    '{ip}'
+                    sens_id
+                ) VALUES (
+                    @temperature,
+                    @humidity,
+                    @pressure,
+                    @date,
+                    @sens_id
                 );
             ";
 
-            SQLiteCommand sqlcmd = _myConnection.CreateCommand();
-            sqlcmd.CommandText = insertCmd;
+            sqlCmd.Parameters.Clear();
+            sqlCmd.Parameters.AddWithValue("@temperature", tv.Value);
+            sqlCmd.Parameters.AddWithValue("@humidity", hv.Value);
+            sqlCmd.Parameters.AddWithValue("@pressure", pv.Value);
+            sqlCmd.Parameters.AddWithValue("@date", dt);
+            sqlCmd.Parameters.AddWithValue("@sens_id", sens_id);
 
-            int rowsInserted = sqlcmd.ExecuteNonQuery();
-
+            sqlCmd.ExecuteNonQuery();
         }
     }
 }

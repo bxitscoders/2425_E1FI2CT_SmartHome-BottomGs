58e4c3fd08dd506f8a90b29aafb8437f0b1ab1d9
ozy <thisisozy.personal@gmail.com>
2025-07-04 08:24:00 +0200

    Reset repo

---
+++ UniversalServer/Model/ConnectionData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace UniversalServer.Model
+{
+    public class ConnectionData
+    {
+        public int Port { get; set; }
+        public IPAddress[] IPAdressList { get; set; }
+        public IPAddress SelectedIPAdress { get; set; }
+    }
+}
